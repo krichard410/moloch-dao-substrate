5a0ce30811f88527ca60be0aa9407cd7eeef6804
Kirsten Richard <kirsten.richard@dal.ca>
2020-07-02 17:56:38 -0300

    This compiles, progress

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -23,41 +23,41 @@ pub trait Trait: system::Trait {
 type ProposalIndex = u32;
 type TokenIndex = u32;
 
+#[derive(Encode, Decode, Clone, Default, PartialEq, Eq)]
+pub struct ProposalInfo<AccountId, BlockNumber> {
+	proposer: AccountId, 
+	starting_period: BlockNumber,
+	yes_votes: u32,
+	no_votes: u32,
+	processed: bool,
+	did_pass: bool,
+	aborted: bool,
+}
+
+
 // #[derive(Encode, Decode, Clone, Default, PartialEq, Eq)]
-// pub struct ProposalInfo<AccountId, BlockNumber> {
+// pub struct base<AccountId, Balance> {
 // 	proposer: AccountId, 
-// 	starting_period: BlockNumber,
-// 	yes_votes: u32,
-// 	no_votes: u32,
-// 	processed: bool,
-// 	did_pass: bool,
-// 	aborted: bool,
+// 	applicant: AccountId,
+// 	sharesRequested: u32,
+// 	tokenTribute: Balance,
 // }
 
-
-#[derive(Encode, Decode, Clone, Default, PartialEq, Eq)]
-pub struct base<AccountId, Balance> {
-	proposer: AccountId, 
-	applicant: AccountId,
-	sharesRequested: u32,
-	tokenTribute: Balance,
-}
-
-#[derive(Encode, Decode, Clone, Default, PartialEq, Eq)]
-pub struct Proposal<Hash, AccountId, Balance, BlockNumber> {
-	base_hash: Hash, //hash of the proposal
-	proposer: AccountId, //proposer accountId, must be a member
-	applicant: AccountId, //applicant accountId, must be a member
-	shares: u32, //number of requested shares
-	startTime: BlockNumber, //when the voting period starts
-	graceStart: Option<BlockNumber>, //when the grace period starts
-	yesVotes: u32, //number of shares that voted yes
-	noVotes: u32, //number of shares that voted no
-	maxVotes: u32, //used to check the number of necessary shares to pass
-	passed: bool,  //if passed, true
-	processed: bool, //if processed, true
-	tokenTribute: Balance,
-}
+// #[derive(Encode, Decode, Clone, Default, PartialEq, Eq)]
+// pub struct Proposal<Hash, AccountId, Balance, BlockNumber> {
+// 	base_hash: Hash, //hash of the proposal
+// 	proposer: AccountId, //proposer accountId, must be a member
+// 	applicant: AccountId, //applicant accountId, must be a member
+// 	shares: u32, //number of requested shares
+// 	startTime: BlockNumber, //when the voting period starts
+// 	graceStart: Option<BlockNumber>, //when the grace period starts
+// 	yesVotes: u32, //number of shares that voted yes
+// 	noVotes: u32, //number of shares that voted no
+// 	maxVotes: u32, //used to check the number of necessary shares to pass
+// 	passed: bool,  //if passed, true
+// 	processed: bool, //if processed, true
+// 	tokenTribute: Balance,
+// }
 
 
 #[derive(Encode, Decode, Default, Clone, PartialEq)]
@@ -74,18 +74,18 @@ pub struct Member {
 
 // This pallet's events.
 decl_event! {
-    pub enum Event<T> where Balance = BalanceOf<T>, <T as system::Trait>::AccountId {
-		// Initialized(AccountId),
-		// CreateProposal(u32, AccountId),
-		// // SubmitVote(proposal_index, sender, unit_vote),
-		// CreateToken(u32, u32),
-		Transfer(AccountId, AccountId, u64),
-		Aborted(Hash, Balance, AccountId, AccountId),
-		Processed(Hash, Balance, AccountId, bool),
-		//Proposal Hash, yes or no vote, yes vote count, no vote count
-		Voted(Hash, bool, u32, u32),
-		//Proposal hash, token tribute, sponsering member, applicant
-		Proposed(Hash, Balance, AccountId, AccountId),
+    pub enum Event<T> where <T as system::Trait>::AccountId {
+		Initialized(AccountId),
+		CreateProposal(u32, AccountId),
+		// SubmitVote(proposal_index, sender, unit_vote),
+		CreateToken(u32, u32),
+		// Transfer(AccountId, AccountId, u64),
+		// Aborted(Hash, Balance, AccountId, AccountId),
+		// Processed(Hash, Balance, AccountId, bool),
+		// //Proposal Hash, yes or no vote, yes vote count, no vote count
+		// Voted(Hash, bool, u32, u32),
+		// //Proposal hash, token tribute, sponsering member, applicant
+		// Proposed(Hash, Balance, AccountId, AccountId),
     }
 }
 
@@ -104,7 +104,7 @@ decl_storage! {
 
 		Members get(fn members): Vec<Member>;
 
-		// Proposals get(fn proposals): map hasher(blake2_128_concat) ProposalIndex => ProposalInfo<T::AccountId, T::BlockNumber>;
+		Proposals get(fn proposals): map hasher(blake2_128_concat) ProposalIndex => ProposalInfo<T::AccountId, T::BlockNumber>;
 
 		ProposalCount get(fn proposal_count): ProposalIndex;
 
@@ -112,22 +112,22 @@ decl_storage! {
 
 		TokenCount get(fn token_count): TokenIndex;
 		
-	 	//Balances get(fn get_balance): map hasher(blake2_128_concat) T::AccountId => u64;
+	 	Balances get(fn get_balance): map hasher(blake2_128_concat) T::AccountId => u64;
 
         TotalSupply get(fn total_supply): u64 = 21000000;
 
 		DidInit get(fn is_init): bool;
 
-		VotingPeriod get(fn voting_period) config(): T::BlockNumber;
-		AbortWindow get(fn abort_window) config(): T::BlockNumber;
-		GracePeriod get(fn grace_period) config(): T::BlockNumber;
-		ProposalFee get(fn proposal_fee) config(): BalanceOf<T>;
-		ProposalBond get(fn proposal_bond) config(): BalanceOf<T>;
+		// VotingPeriod get(fn voting_period) config(): T::BlockNumber;
+		// AbortWindow get(fn abort_window) config(): T::BlockNumber;
+		// GracePeriod get(fn grace_period) config(): T::BlockNumber;
+		// ProposalFee get(fn proposal_fee) config(): BalanceOf<T>;
+		// ProposalBond get(fn proposal_bond) config(): BalanceOf<T>;
 
-		Proposals get(fn proposals): map hasher(blake2_128_concat) T::Hash => Proposal<T::Hash, T::AccountId, BalanceOf<T>, T::BlockNumber>; 
+		// Proposals get(fn proposals): map hasher(blake2_128_concat) T::Hash => Proposal<T::Hash, T::AccountId, BalanceOf<T>, T::BlockNumber>; 
 
-		ProposalsFor get(fn proposals_for): map hasher(blake2_128_concat) T::AccountId => Vec<T::Hash>;
-		VotersFor get(fn voters_for): map hasher(blake2_128_concat) T::Hash => Vec<T::AccountId>;
+		// ProposalsFor get(fn proposals_for): map hasher(blake2_128_concat) T::AccountId => Vec<T::Hash>;
+		// VotersFor get(fn voters_for): map hasher(blake2_128_concat) T::Hash => Vec<T::AccountId>;
     }
 }
 // The pallet's dispatchable functions.
@@ -146,84 +146,84 @@ decl_module! {
 
 		#[weight = 10_000]
 		fn init(origin) {
-			// let sender = ensure_signed(origin)?;
-			// ensure!(!DidInit::exists(), <Error<T>>::AlreadyInitialized);
+			let sender = ensure_signed(origin)?;
+			ensure!(!DidInit::exists(), <Error<T>>::AlreadyInitialized);
 
-			// // TODO. Rewrite this part
-			// let mut current_members = Self::members();
+			// TODO. Rewrite this part
+			let mut current_members = Self::members();
 
-			// let new_member = Member {
-			// 	exists: true,
-			// 	highest_index_yes_vote: 0,
-			// };
+			let new_member = Member {
+				exists: true,
+				highest_index_yes_vote: 0,
+			};
 
-			// current_members.insert(0, new_member);
-			// Members::put(current_members);
-			// <Balances<T>>::insert(sender, Self::total_supply());
-			// DidInit::put(true);
+			current_members.insert(0, new_member);
+			Members::put(current_members);
+			<Balances<T>>::insert(sender, Self::total_supply());
+			DidInit::put(true);
 		}
 
 		#[weight = 10_000]
 		fn create_proposal(origin) {
-			// let sender = ensure_signed(origin)?;
-			// //ensure!(DidInit::exists() && Self::is_init(), <Error<T>>::NotInit);
+			let sender = ensure_signed(origin)?;
+			//ensure!(DidInit::exists() && Self::is_init(), <Error<T>>::NotInit);
 
-			// let starting_period;
-			// starting_period = <system::Module<T>>::block_number() + T::StartingPeriod::get();
+			let starting_period;
+			starting_period = <system::Module<T>>::block_number() + T::StartingPeriod::get();
 
-			// let index = ProposalCount::get();
-			// let next_index = index.checked_add(1).ok_or(Error::<T>::Overflow)?;
-			// ProposalCount::put(next_index);
+			let index = ProposalCount::get();
+			let next_index = index.checked_add(1).ok_or(Error::<T>::Overflow)?;
+			ProposalCount::put(next_index);
 
-			// let index_token = TokenCount::get();
-			// let following_index = index_token.checked_add(1).ok_or(Error::<T>::Overflow)?;
-			// TokenCount::put(following_index);
+			let index_token = TokenCount::get();
+			let following_index = index_token.checked_add(1).ok_or(Error::<T>::Overflow)?;
+			TokenCount::put(following_index);
 
-			// let new_proposal = ProposalInfo {
-			// 	proposer: sender.clone(),
-			// 	starting_period: starting_period,
-			// 	// TODO. Write another module for the token creation
-			// 	yes_votes: 0,
-			// 	no_votes: 0,
-			// 	processed: false,
-			// 	did_pass: false,
-			// 	aborted: false,
-			// };
+			let new_proposal = ProposalInfo {
+				proposer: sender.clone(),
+				starting_period: starting_period,
+				// TODO. Write another module for the token creation
+				yes_votes: 0,
+				no_votes: 0,
+				processed: false,
+				did_pass: false,
+				aborted: false,
+			};
 
-			// let new_tokens = TokenCreation {
-			// 	tribute_token: 0,
-			// 	payment_token: 0,
-			// };
+			let new_tokens = TokenCreation {
+				tribute_token: 0,
+				payment_token: 0,
+			};
 
-			// <Tokens>::insert(index_token, new_tokens);
-			// <Proposals<T>>::insert(index, new_proposal);
-			//Self::deposit_event(RawEvent::CreateProposal(index, sender));
+			<Tokens>::insert(index_token, new_tokens);
+			<Proposals<T>>::insert(index, new_proposal);
+			Self::deposit_event(RawEvent::CreateProposal(index, sender));
 			//Self::deposit_event(RawEvent::CreateToken(index, sender));
 		}
 
-		#[weight = 10_000]
-		fn propose(origin, applicant: AccountId, shares: u32, tokenTribute: BalanceOf<T>) {
-			let sender = ensure_signed(origin)?;
-			ensure!(Self::is_member(&sender), "proposer is not a member of the Dao");
+		//#[weight = 10_000]
+		// fn propose(origin, applicant: AccountId, shares: u32, tokenTribute: BalanceOf<T>) {
+		// 	let sender = ensure_signed(origin)?;
+		// 	ensure!(Self::is_member(&sender), "proposer is not a member of the Dao");
 
-			let prop = Proposal::new(&sender, &applicant, shares, tokenTribute, time);
+		// 	let prop = Proposal::new(&sender, &applicant, shares, tokenTribute, time);
 
-			Self::proposals::insert(prop.base_hash, prop);
+		// 	Self::proposals::insert(prop.base_hash, prop);
 
-			// add if member is sponsoring a proposal
-			Self::proposals_for::mutate(&sender, |props| props.push(prop.base_hash));
+		// 	// add if member is sponsoring a proposal
+		// 	Self::proposals_for::mutate(&sender, |props| props.push(prop.base_hash));
 		
-		}
+		// }
 
-		#[weight = 10_000]
-		fn vote(origin, hash: Hash, approve: bool) {
-			let sender = ensure_signed(origin)?;
+		// #[weight = 10_000]
+		// fn vote(origin, hash: Hash, approve: bool) {
+		// 	let sender = ensure_signed(origin)?;
 
-		}
+		// }
 
-		#[weight = 10_000]
-		fn process(origin, hash: Hash) {
-		}
+		// #[weight = 10_000]
+		// fn process(origin, hash: Hash) {
+		// }
 
 		#[weight = 10_000]
 		fn voting_period(origin) {
@@ -291,9 +291,9 @@ decl_module! {
 		#[weight = 10_000]
 		fn rage_quit(origin){
 			let sender = ensure_signed(origin)?;
-			ensure!(Self::is_member(&sender), "proposer is not a member of DAO");
+			//ensure!(Self::is_member(&sender), "proposer is not a member of DAO");
 
-			ensure!(Self::proposals_for::get(&sender).is_empty(), "all proposals have not passed or exited the grace period");
+			//ensure!(Self::proposals_for::get(&sender).is_empty(), "all proposals have not passed or exited the grace period");
 			
 			//TODO add more details for storage here
 
